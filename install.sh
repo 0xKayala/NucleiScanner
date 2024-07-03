1d0f3418498b67d3feda44720f92f7dd0905834a
Satya Prakash <satyaprakash99915@gmail.com>
2024-07-03 16:41:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,19 @@ sudo mv NucleiScanner.sh /usr/bin/ns
 # Make the NucleiScanner file executable
 sudo chmod u+x /usr/bin/ns
 
-# Remove the NucleiScanner folder from the home directory
-if [ -d "$home_dir/NucleiScanner" ]; then
-    echo "Removing NucleiScanner folder..."
-    rm -r "$home_dir/NucleiScanner"
+# Path to the file
+FILE="$HOME/.gau.toml"
+
+# URL of the ".gau.toml" file to download
+URL="https://raw.githubusercontent.com/lc/gau/master/.gau.toml"
+
+# Check if the file ".gau.toml" exists
+if [ -f "$FILE" ]; then
+    echo "$FILE already exists."
+else
+    echo "$FILE does not exist. Downloading the file"
+    curl -o "$FILE" "$URL"
+    echo "Downloaded $FILE."
 fi
 
 echo "NucleiScanner has been installed successfully! Now Enter the command 'ns' to run the tool."
